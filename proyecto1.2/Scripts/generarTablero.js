3f9e1519a1c557ddeca22c4e574fa4a206abd41e
alejandro <alejandro.meno92@gmail.com>
2023-01-27 16:06:19 +0100

    rand item generator for map

--- proyecto1.2/Scripts/generarTablero.js
+++ proyecto1.2/Scripts/generarTablero.js
@@ -2,14 +2,25 @@ function generartablero(){
     var cantidad=64;//document.getElementById('cantidad').value;
     var alto=110 //ò document.getElementById('alto').value;
     var ancho=110  //ó document.getElementById('ancho').value;
+    var auxiliarRand = 0;
     document.getElementById('cuadros').innerHTML='';
     document.getElementById('cuadros').style.width = ((2 + parseInt(ancho)) * 8) + "px";
     
     for (var i=0;i<cantidad;i++){
         if(Math.floor(i/8)%2 == i%2){
-            document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:rgb(126, 196, 126);float:left;"><div>';
+            auxiliarRand = Math.floor(Math.random() * 10);
+            if(auxiliarRand == 2){
+                document.getElementById('cuadros').innerHTML+=
+            '<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +
+            'px;background-color:rgb(126, 196, 126);float:left;"><img src="../assets/gameAssets/gameImages/rocks.png" alt="rock"></div>';
+            }
+            else{
+                document.getElementById('cuadros').innerHTML+=
+            '<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +
+            'px;background-color:rgb(126, 196, 126);float:left;"></div>';
+            }
         }else{
-            document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:rgb(62, 74, 62);float:left;"><div>';
+            document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:rgb(62, 74, 62);float:left;"></div>';
     
         }
         }
